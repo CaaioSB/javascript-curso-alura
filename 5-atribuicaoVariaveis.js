b7e9f17b0db95f27464228d23272442bdcca7921
Caio da Silva Batista <caio_silvabatista@hotmail.com>
2020-08-13 21:26:54 -0300

    feat(variable): works with variable assignment

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,13 @@
+console.log('Trabalhando com atribução de variáveis')
+
+const idade = 19
+const nome = 'Caio'
+const sobrenome = 'Silva'
+
+// console.log(nome + ' ' + sobrenome)
+console.log(nome, sobrenome)
+console.log(`Meu nome é: ${nome} ${sobrenome}`)
+
+nome = nome + sobrenome
+
+console.log(nome)
